2549777fd197e3f8008072959d9824a9accafe10
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-08 23:23:31 -0400

    added floor block, added obstacle block with spinning block

---
+++ src/Level.jsx
@@ -0,0 +1,84 @@
+import { useRef, useState } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody } from "@react-three/rapier";
+
+// fixes colour when r3f used
+THREE.ColorManagement.legacyMode = false;
+
+// on geomtry used
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+// materials for various objects
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+// function adds starting blocks
+function BlockStart({ position = [0, 0, 0] }) {
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+    </group>
+  );
+}
+
+// function adds spinner block
+function BlockSpinner({ position = [0, 0, 0] }) {
+  const obstacle = useRef();
+  const [speed] = useState(
+    () => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1)
+  );
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const rotation = new THREE.Quaternion();
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
+    obstacle.current.setNextKinematicRotation(rotation);
+  });
+
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor2Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+
+      <RigidBody
+        ref={obstacle}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
+
+export default function Level() {
+  return (
+    <>
+      <BlockStart position={[0, 0, 4]} />
+      <BlockSpinner position={[0, 0, 0]} />
+    </>
+  );
+}
