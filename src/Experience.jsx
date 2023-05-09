e0bdc7f0098211f22f62435556fff6d8b7a8f13f
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-09 12:49:33 -0400

    added ball for player movement, forward,backward,left,right, jump, added logic to jump not on ech frame but when it touches ground, using raycaster

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,6 +3,7 @@ import { Physics, Debug } from "@react-three/rapier";
 
 import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
+import Player from "./Player.jsx";
 
 export default function Experience() {
   return (
@@ -10,10 +11,11 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <Physics>
-        <Debug />
+        {/* <Debug /> */}
 
         <Lights />
         <Level />
+        <Player />
       </Physics>
     </>
   );
--- src/Level.jsx
+++ src/Level.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useMemo } from "react";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import { useGLTF } from "@react-three/drei";
 
 // fixes colour when r3f used
@@ -195,6 +195,48 @@ export function BlockAxe({ position = [0, 0, 0] }) {
   );
 }
 
+function Bounds({ length = 1 }) {
+  return (
+    <>
+      <RigidBody type="fixed" restitution={0.2} friction={0}>
+        {/* right wall */}
+        <mesh
+          position={[2.15, 0.75, -(length * 2) + 2]}
+          geometry={boxGeometry}
+          material={wallMaterial}
+          scale={[0.3, 1.5, 4 * length]}
+          castShadow
+        />
+
+        {/* left wall */}
+        <mesh
+          position={[-2.15, 0.75, -(length * 2) + 2]}
+          geometry={boxGeometry}
+          material={wallMaterial}
+          scale={[0.3, 1.5, 4 * length]}
+          receiveShadow
+        />
+
+        {/* end wall */}
+        <mesh
+          position={[0, 0.75, -(length * 4) + 2]}
+          geometry={boxGeometry}
+          material={wallMaterial}
+          scale={[4, 1.5, 0.3]}
+          receiveShadow
+        />
+
+        <CuboidCollider
+          args={[2, 0.1, 2 * length]}
+          position={[0, -0.1, -(length * 2) + 2]}
+          restitution={0.2}
+          friction={1}
+        />
+      </RigidBody>
+    </>
+  );
+}
+
 export function Level({
   count = 5,
   types = [BlockSpinner, BlockAxe, BlockLimbo],
@@ -218,7 +260,9 @@ export function Level({
         <Block key={index} position={[0, 0, -(index + 1) * 4]} />
       ))}
 
-      <BlockEnd position={[0, 0, - (count + 1) * 4]} />
+      <BlockEnd position={[0, 0, -(count + 1) * 4]} />
+
+      <Bounds length={count + 2} />
     </>
   );
 }
