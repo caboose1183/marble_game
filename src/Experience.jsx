3d9773f36b236109c7d13ed6eabf2b3daaedb04c
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-09 20:01:37 -0400

    added reflections, changed colours

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,6 +3,7 @@ import { Physics, Debug } from "@react-three/rapier";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import Player from "./Player.jsx";
+import Effects from "./Effects.jsx";
 
 import useGame from "./stores/useGame.js";
 
@@ -16,7 +17,7 @@ export default function Experience() {
 
   return (
     <>
-      <color args={["#bdedfc"]} attach="background" />
+      <color args={["#252731"]} attach="background" />
 
       <Physics>
         {/* <Debug /> */}
@@ -25,6 +26,8 @@ export default function Experience() {
         <Level count={blocksCount} seed={blocksSeed} />
         <Player />
       </Physics>
+
+      <Effects />
     </>
   );
 }
